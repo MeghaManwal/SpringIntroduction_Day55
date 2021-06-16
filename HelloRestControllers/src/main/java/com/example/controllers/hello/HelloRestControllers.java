994ed6829e6983df20ef8d93f3d8ea5ebfd0c62b
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-06-16 10:20:07 +0530

    Added: To show message using GET request Method with name as query parameter

--- HelloRestControllers/src/main/java/com/example/controllers/hello/HelloRestControllers.java
+++ HelloRestControllers/src/main/java/com/example/controllers/hello/HelloRestControllers.java
@@ -1,15 +1,29 @@
 package com.example.controllers.hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloRestControllers {
 	
+	/*UC-1  (command: curl -X GET "http://localhost:8080/hello" -w "\n")
+	        (url:     http://localhost:8080/hello) */
+	
 	@RequestMapping(value = {"","/","/home"})
 	public static String sayHello() {
 		return "Hello From Bridgelabz!!";
 	}
+	
+	/* UC-2  (command: curl "http://localhost:8080/hello/query?name=Megha" -w "\n")
+	         (url:     http://localhost:8080/hello/query?name=Megha) */
+	
+	@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value="name")String name) {
+		return "Hello" + name + " !!";
+	}
 
 }
+ 
