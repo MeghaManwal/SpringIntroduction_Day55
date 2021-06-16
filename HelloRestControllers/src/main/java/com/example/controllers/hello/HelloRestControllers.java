dc088d0355f49eaf3c9bdd9560fbd2ba76e6ac52
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-06-16 11:03:27 +0530

    Added: To show message using POST request Method and pass name in the body

--- HelloRestControllers/src/main/java/com/example/controllers/hello/HelloRestControllers.java
+++ HelloRestControllers/src/main/java/com/example/controllers/hello/HelloRestControllers.java
@@ -2,10 +2,13 @@ package com.example.controllers.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.controllers.hello.User;
 
 @RestController
 @RequestMapping("/hello")
@@ -33,6 +36,14 @@ public class HelloRestControllers {
 	@GetMapping ("/param/{name}")
 	public String sayHelloParam(@PathVariable String name) {
 		return "Hello " + name + " !!"; 
+	}
+	
+	/* UC-4 (command: )
+	        (url: ) */
+	
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName() + " !!";
 	}
 }
  
